40609128ffc8651bf280d259e78cf5a56e7c6c2a
Vladimir Kopytok <kopytokv@gmail.com>
2018-08-27 22:54:52 +0300

    Started pruning. More logging

--- sparse_dynamic_programming.py
+++ sparse_dynamic_programming.py
@@ -1,3 +1,4 @@
+import time
 import logging
 from copy import deepcopy
 from collections import namedtuple
@@ -7,13 +8,17 @@ import pandas as pd
 
 from scipy.sparse import vstack, csr_matrix
 
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.DEBUG,
     format="%(levelname)s - %(asctime)s - %(msg)s",
-    datefmt="%Y-%m-%d %H:%M:%S")
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler("text_log.log"), # For debug
+        logging.StreamHandler(),
+    ])
 
 def line_to_numbers(line):
     """ Split line into 2 digits and convert them to int """
-    return tuple(int(num) for num in line.split())
+    return tuple(map(int, line.split()))
 
 def read_item(line):
     """ Read one knapsack item """
@@ -26,8 +31,9 @@ def prepare_items(items=None):
     if items:
         df = pd.DataFrame(items)
         by = "density" if df["density"].std() > 0.2 else "value"
+        else_by = "density" if by != "density" else "value"
         logging.info("Sorted by {}".format(by))
-        df.sort_values(by, ascending=False, inplace=True)
+        df.sort_values([by, else_by], ascending=False, inplace=True)
         df["select"] = np.nan
         logging.info("First 5 items:\n{}".format(df.head()))
         return df
@@ -43,14 +49,16 @@ class Domain(object):
 
         self.numbers = np.linspace(0, capacity, capacity + 1)
         self.grid = csr_matrix((1, capacity + 1))
+        self.cur_item_id = 0
+
         self.result = 0
 
     def __len__(self):
         return self.grid.shape[0] - 1
 
     def __repr__(self):
-        return "Knapsack. Capacity: {}, items: checked {}\{}"\
-            .format(self.capacity, len(self), self.n_items)
+        return "Knapsack. Capacity: {}, items: {}"\
+            .format(self.capacity, self.n_items)
 
     def get_row(self, row_ix):
         """ Convert row from sparse into np.array """
@@ -63,46 +71,69 @@ class Domain(object):
         new_state = np.where(mask, row, 0)
         self.grid = vstack([self.grid, new_state]).tocsr()
 
-    def add_item(self, item):
+    def add_item(self, n, item):
         """ Evaluate item and expand grid """
         state = self.get_row(-1)
 
-        # Evaluate
-        item_value = np.where(self.numbers > item.weight, item.value, 0)
-
-        if item.weight < self.capacity:
-            weight = int(item.weight)
-            shifted_state = np.hstack([[0] * weight, (state + item.value)[:-weight]])
-            new_state = np.max([state, shifted_state], axis=0)
+        weight = int(item.weight)
+        if weight < self.capacity:
+            item_value = np.where(self.numbers > weight, item.value, 0)
+            if_add = np.hstack(
+                [state[:weight], (state + item.value)[:-weight]])
+            new_state = np.max([state, if_add], axis=0)
+            self.add_row(new_state)
+            if (new_state[:-weight] != state[:-weight]).all():
+                logging.info("Filled 1 for {}".format(n))
+                self.items.loc[n, "select"] = 1
         else:
-            new_state = state
-
-        # Add row to domain
-        self.add_row(new_state)
+            logging.info("Filled 0 for {}".format(n))
+            self.items.loc[n, "select"] = 0
 
     def forward(self):
         """ Fill domain """
         for n, item in self.items.iterrows():
-            self.add_item(item)
+            self.cur_item_id = n
+            if np.isnan(item["select"]):
+                self.add_item(n, item)
 
     def backward(self):
         """ Find answer using filled domain """
-        prev = self.get_row(-1)
-        ix = np.argmax(prev)
-        self.result = np.max(prev)
+        last_row = self.get_row(-1)
+        ix = np.argmax(last_row) # First weight with max value
+        self.result = int(np.max(last_row))
 
-        for i in range(self.n_items-1, -1, -1):
-            cur = self.get_row(i)
-            changed = (cur[ix] != prev[ix])
-            self.items.loc[i, "select"] = changed
-            ix -= int(self.items.loc[i, "weight"]) if changed else 0
-            prev = cur
+        for n, (i, item) in enumerate(self.items.iloc[::-1, :].iterrows()):
+            weight = int(self.items.loc[i, "weight"])
+            if np.isnan(item["select"]):
+                cur = self.get_row(-n)
+                prev = self.get_row(-(n - 1))
+                if cur[ix] == prev[ix] == 0:
+                    break
+                logging.debug("cur[ix]: {}".format(cur[ix]))
+                logging.debug("prev[ix]: {}".format(prev[ix]))
+                self.items.loc[i, "select"] = (cur[ix] != prev[ix])
+
+            select = self.items.loc[i, "select"]
+            if select:
+                logging.debug("Select item:\n{}".format(item))
+            ix -= weight if select else 0
+            logging.debug("ix: {}".format(ix))
+            if ix == 0:
+                break
+
+        self.items["select"].fillna(0, inplace=True)
         return self.items.sort_index()["select"].astype(int).tolist()
 
     def solve(self):
+        t0 = time.time()
+        logging.info("Filling domain")
         self.forward()
         logging.info("Finished filling domain")
-        return self.backward()
+        answer = self.backward()
+        logging.info("Finished in (sec): {}".format(time.time() - t0))
+        logging.info("Resulting value: {}".format(self.result))
+        logging.info("Selected items: {}".format(answer))
+        return answer
 
     @classmethod
     def load(cls, path):
@@ -150,13 +181,10 @@ def main():
     data_folder = "data"
     filename = select_file(data_folder)
     path = op.join(data_folder, filename)
-    
+
     d = Domain().load(path)
     t0 = time.time()
     answer = d.solve()
-    logging.info("Finished in (sec): {}".format(time.time() - t0))
-    logging.info("Resulting value: {}".format(d.result))
-    logging.info("Selected items: {}".format(answer))
 
 if __name__=="__main__":
     main()
