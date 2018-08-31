Code cleaning. Fixed result calculation

prune.py
@@ -77,17 +77,6 @@ def prune_clean_backward(knapsack, order):
     logging.debug("Number of items in domain: {}\tdomain shape: {}"
         .format(knapsack.grid.count_nonzero(), knapsack.grid.shape))
 
-def prune_clean_one(knapsack, order):
-    """ Remove row with index order from domain """
-    knapsack.grid = vstack([
-        knapsack.grid[:order, :],
-        knapsack.grid[order+1:, :]
-    ]).tolil()
-    knapsack.items.loc[knapsack.items["order"] == order, "take"] = 0 # Redundant
-    knapsack.items.loc[knapsack.items["order"] == order, "order"] = np.nan
-    logging.debug("Removed row {} from domain, shape: {}"
-        .format(order, knapsack.grid.shape))
-
 def prune_remove_not_taken(knapsack):
     """ Remove from domain rows for items with "take" == 0 """
     prune_incomming_not_taken(knapsack)
@@ -95,12 +84,14 @@ def prune_remove_not_taken(knapsack):
 
 def prune_incomming_not_taken(knapsack):
     """ Decrease number of rows in domain by number of not taken items """
-    ix = (knapsack.items[["take", "prune"]] == 0).all(1)
-    subtrahend = ix.sum()
-    if subtrahend:
-        knapsack.items.loc[ix, "prune"] = 1
-        knapsack.grid = lil_matrix(knapsack.grid[:-subtrahend, :])
-        logging.debug("Decreased domain n_rows by {}".format(subtrahend))
+    # TODO Analyze
+    pass
+    # ix = (knapsack.items[["take", "prune"]] == 0).all(1)
+    # subtrahend = ix.sum()
+    # if subtrahend:
+    #     knapsack.items.loc[ix, "prune"] = 1
+    #     knapsack.grid = lil_matrix(knapsack.grid[:-subtrahend, :])
+    #     logging.debug("Decreased domain n_rows by {}".format(subtrahend))
 
 def prune_observed_not_taken(knapsack):
     """ Remove from domain rows for observed not taken items """

sparse_dynamic_programming.py
@@ -119,7 +119,7 @@ class Knapsack(object):
 
     def get_result(self):
         """ Return sum of values of taken items """
-        return self.items.loc[np.where(self.items["take"])[0], "value"].astype(int).sum()
+        return self.items.loc[self.items["take"] == 1, "value"].astype(int).sum()
 
     def eval_left(self, param="value", order=None):
         """ Return sum of param for untouched items """
@@ -177,6 +177,9 @@ class Knapsack(object):
                 order += 1
             else:
                 self.items.loc[cur_id, "order"] = np.nan
+                self.items.loc[cur_id, "take"] = 0
+            logging.debug("Number of unsolved items: {}".format(
+                self.items[["order", "take"]].isnull().all(1).sum()))
             logging.debug("Number of items in domain: {}"
                 .format(self.grid.count_nonzero()))
             self.prune()
@@ -191,7 +194,6 @@ class Knapsack(object):
 
         while ix > 0:
             col = self.grid.tocsr()[:, :ix+1].max(1)
-            logging.debug("col:\n{}".format(col))
             order = np.argmax(col)
             item_id = self.items["order"] == order
             logging.debug("Take item with order {}".format(order))