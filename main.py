24c15a3d7ff898b1134557736382b5c93f994b1a
Kopytok <kopytokv@gmail.com>
2019-05-28 21:39:46 +0300

    Removed csv knapsack

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import pandas as pd
 
 from knapsack.imports import *
+from knapsack.open_data import select_file_in
 from knapsack.knapsack import Knapsack
 
 logging.basicConfig(level=logging.INFO,
     format="%(levelname)s - %(asctime)s - %(msg)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
-        logging.FileHandler("log_knapsack.log"), # For debug
+#        logging.FileHandler("log_knapsack.log"), # For debug
         logging.StreamHandler(),
     ])
 
 def main():
     """ Usage example """
-    items = pd.read_csv("200_items.csv")
-    capacity = 2640230
-
-    knapsack = Knapsack(capacity, items)
+    path = select_file_in("data")
+    knapsack = Knapsack.load(f"data/{path}")
     answer = knapsack.solve()
     print("Total value of selected items: {}".format(knapsack.result))
 
